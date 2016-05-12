22c1a3b579025b9048c0c117ccbe9fb236686f79
ChadYin <996707589@qq.com>
2016-05-12 17:20:59 +0800

    springmvchello

---
+++ src/com/yc/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.yc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0,
+			HttpServletResponse arg1) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("msg", "helloworld");
+		mv.setViewName("hello");
+		return mv;
+	}
+
+}
